bfe2e667a41ad78c84e2f68932b1075e374da381
Ory Band <oryband@gmail.com>
2018-02-04 15:00:11 +0200

    add cmd/info app
    prints account info according to given address

---
+++ cmd/info/main.go
@@ -0,0 +1,38 @@
+// Print account info for given address
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/stellar/go/clients/horizon"
+)
+
+var (
+	horizonDomainFlag = flag.String("horizon", "https://horizon-testnet.stellar.org", "horizon address")
+	addressFlag       = flag.String("address", "", "account address")
+)
+
+func main() {
+	flag.Parse()
+
+	client := horizon.Client{
+		URL:  *horizonDomainFlag,
+		HTTP: &http.Client{Timeout: 5 * time.Second},
+	}
+
+	account, err := client.LoadAccount(*addressFlag)
+	if err != nil {
+		panic(err)
+	}
+
+	b, err := json.Marshal(&account)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(b))
+}
