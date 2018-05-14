aade815654ea061a6a9d1ac45819dd1c4da9e85a
ashevin <avi.shevin@kik.com>
2018-05-14 14:14:04 +0300

    Usage changes for loadtest.sh.
    1) accounts file must be in the current directory.
    2) accounts file must be named <count>.json, where <count> = the number of key-pairs in the file.
    3) loadtest.sh must be passed the count to be used to identify the accounts file.
    4) the number of submitters may be passed as a second parameter, and defaults to <count> if not given.

--- scripts/loadtest.sh
+++ scripts/loadtest.sh
@@ -4,15 +4,26 @@
 set -x
 set -e
 
+COUNT=$1
+
+if [ "$COUNT" == "" ]; then
+	echo "Usage: $0 <COUNT>"
+	exit 1
+fi
+
+SUBMITTERS=$2
+if [ "$SUBMITTERS" == "" ]; then
+	SUBMITTERS="${COUNT}"
+fi
+
 DEBUG="${DEBUG:-true}"
 HORIZON="${HORIZON:-http://localhost:8000}"
-PASSPHRASE="${PASSPHRASE:-'private testnet'}"
+PASSPHRASE="${PASSPHRASE:-"private testnet"}"
 LOG="${LOG:-loadtest.log}"
-ACCOUNTS_FILE="${ACCOUNTS_FILE:-accounts.json}"
+ACCOUNTS_FILE="${COUNT}.json"
 TX_AMOUNT="${TX_AMOUNT:-0.0001}"
 OPS_PER_TX="${OPS_PER_TX:-1}"
 TIME_LENGTH="${TIME_LENGTH:-20}"
-SUBMITTERS="${SUBMITTERS:-100}"
 RATE="${RATE:-0}"  # zero disables rate limiting
 BURST="${BURST:-100}"
 
