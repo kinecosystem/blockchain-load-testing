29d9a0f6869f531f16cfd4988eb2ba1aef1b05d9
ittiel <ittiel@gmail.com>
2019-07-31 10:50:23 +0300

    adjusting the test default values

--- scripts/perf-logs.sh
+++ scripts/perf-logs.sh
@@ -5,7 +5,7 @@
 for core in $CORE_SERVERS;
 do
     echo $core
-    ssh -o "StrictHostKeyChecking=no" -i $SSH_KEY ubuntu@$core "cd /data/ ; sudo su -c 'docker-compose logs --no-color --tail=300000 stellar-core | gzip > /tmp/$core-log.json.gz'" && \
+    ssh -o "StrictHostKeyChecking=no" -i $SSH_KEY ubuntu@$core "cd /data/ ; sudo su -c 'docker-compose logs --no-color  stellar-core | gzip > /tmp/$core-log.json.gz'" && \
     scp -i $SSH_KEY ubuntu@$core:/tmp/$core-log.json.gz /tmp/$core-$1-$2-log.json.gz &
 done
 
@@ -16,7 +16,7 @@ sleep 60
 HORIZON_DOMAIN=$(echo "$HORIZON" | awk -F/ '{print $3}')
 echo "getting data from $HORIZON_DOMAIN"
 ssh -i $SSH_KEY ubuntu@$HORIZON_DOMAIN  "sudo docker exec data_horizon-db_1 psql -h localhost -U stellar horizon -c 'select transaction_hash, ledger_sequence from history_transactions where ledger_sequence >= 1000 and ledger_sequence <= 2000' " 1> /tmp/perf-tx-ledgers.txt
-ssh -i $SSH_KEY ubuntu@$HORIZON_DOMAIN  "sudo su -c 'docker logs horizon  --tail 1000000 2> /tmp/a'; grep \"Finished ingesting ledgers\" /tmp/a "  | tail -300 > /tmp/perf-horizon-ingest.log
+ssh -i $SSH_KEY ubuntu@$HORIZON_DOMAIN  "sudo su -c 'docker logs horizon   2> /tmp/a'; grep \"Finished ingesting ledgers\" /tmp/a "  | tail -300 > /tmp/perf-horizon-ingest.log
 
 wait
 exit 0
--- vars.sh
+++ vars.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 #IMPORTANNT: FUNDER_SEED and CHANNEL_SEED will be overridden if activated from Jenkins
 
+#needs to be lower than AMOUNT_OF_SOURCE_ACCOUNTS
 export RATE=80
 export REPETITIONS=1
 export TIME_LENGTH=600
 
-export AMOUNT_OF_SOURCE_ACCOUNTS=10
+#max amount of simultaneous tx
+export AMOUNT_OF_SOURCE_ACCOUNTS=1000
 export ACCOUNTS_FILE=accounts.json
 export GOPATH=~/go
-export AMOUNT_OF_DESTINATION_ACCOUNTS=2
+export AMOUNT_OF_DESTINATION_ACCOUNTS=20
 export DEST_ACCOUNT=dest.json
 
 export OPS_PER_TX=1
-export SUBMITTERS=2000
+#less than or equal to the AMOUNT_OF_SOURCE_ACCOUNTS
+export SUBMITTERS=100
 
 export CORE_SERVERS="ip-core-test-1.test.kin ip-core-test-2.test.kin  ip-core-test-3.test.kin ip-core-test-4.test.kin ip-core-test-5.test.kin"
 export HORIZON=http://ip-horizon-test-1.test.kin
@@ -20,7 +23,6 @@ export HORIZON=http://ip-horizon-test-1.test.kin
 
 export S3_BUCKET_NAME="perf-test-s3-logs"
 
-#CORE_SERVERS="p    erf-1 perf-2 perf-3 perf-4 perf-5 perf-6 perf-7 perf-8 perf-9 perf-10 perf-11"
 export PASSPHRASE="Kin test ; Jul 2019"
 
 #test seeds
